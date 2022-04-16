8fa2f6c560c5d02a55bd334d347653c198cd955a
刘久武 <jiuwu.liu@thoughtworks.com>
2022-04-16 18:27:08 +0800

    init

--- src/test/java/de/rieckpil/blog/repository/BookRepositoryShortTest.java
+++ src/test/java/de/rieckpil/blog/repository/BookRepositoryShortTest.java
@@ -9,16 +9,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest(properties = {
         "spring.test.database.replace=NONE",
-        //"spring.datasource.url=jdbc:tc:mysql:5.7:///springboot"
-        "spring.datasource.url=jdbc:tc:mysql:8:///springboot",
-        //"spring.datasource.url=jdbc:tc:postgresql:12:///springboot"
+        //"spring.datasource.url=jdbc:tc:mysql:5.7:///test"
+        "spring.datasource.url=jdbc:tc:mysql:8:///test",
 })
 public class BookRepositoryShortTest {
     @Autowired
     private BookRepository bookRepository;
 
     @Test
-    @Sql("/scripts/INIT_TWO_BOOK.sql")
+    @Sql("/scripts/INIT_THREE_BOOK.sql")
     public void testCustomNativeQuery() {
         assertEquals(3, bookRepository.findAll().size());
     }
--- src/test/resources/scripts/INIT_TWO_BOOK.sql
+++
@@ -1,3 +0,0 @@
-insert into books (name) values ("Rust");
-insert into books (name) values ("Java");
-insert into books (name) values ("Golang");
